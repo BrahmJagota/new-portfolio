0ebe7b3b327bd9baa7b17680abe3509e018891f0
unknown <jagotabrahm@gmail.com>
2025-05-21 16:20:14 +0530

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import ContactMe from "./components/ContactMe";
 import Projects from "./components/Projects";
 import Hero from "./components/Hero";
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Projects />
       <ContactMe />
+       <Analytics />
     </div>
   );
 }
